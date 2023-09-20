d9efb3a0dc6ae4bca2eefcb0f1e1f55f0343cadd
John <128350391+alpaOo@users.noreply.github.com>
2023-09-21 00:35:57 +0800

    Update nav bar functionality   - add modules for Home, Menu, and about   - each module contain the contents for their respective sections   - barebones page styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 "use strict";
 
 import { mainPage } from "./main-page";
+import { navHome } from "./nav-home";
+import { navMenu } from "./nav-menu";
+import { navAbout } from "./nav-about";
 import './style.css';
 
-const { pageTitle, navBar } = mainPage();
+document.addEventListener('DOMContentLoaded', function () {
+  const mainContent = document.getElementById('content');
+  const { pageTitle, navBar } = mainPage();
 
-document.body.appendChild(pageTitle);
-document.body.appendChild(navBar);
+  mainContent.appendChild(pageTitle);
+  mainContent.appendChild(navBar);
+  mainContent.appendChild(navHome());
+
+  navBar.addEventListener('click', function (e) {
+    if (mainContent.lastChild && e.target instanceof HTMLLIElement && mainContent.lastChild instanceof HTMLDivElement) {
+      mainContent.removeChild(mainContent.lastChild);
+    }
+    if (e.target.textContent === 'Home') {
+      mainContent.appendChild(navHome());
+    } else if (e.target.textContent === 'Menu') {
+      mainContent.appendChild(navMenu());
+    } else if (e.target.textContent === 'About') {
+      mainContent.appendChild(navAbout());
+    }
+  });
+})
+
---
+++ src/nav-menu.js
@@ -0,0 +1,12 @@
+"use strict";
+
+export function navMenu() {
+  const container = document.createElement('div');
+  const content = document.createElement('p');
+
+  container.className = 'nav-menu-content';
+  content.textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut pharetra sit amet aliquam id. Fusce id velit ut tortor pretium viverra suspendisse potenti nullam. Ultrices mi tempus imperdiet nulla. Scelerisque eleifend donec pretium vulputate. Risus quis varius quam quisque. Sed enim ut sem viverra aliquet eget sit amet. Diam quam nulla porttitor massa id neque aliquam. Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc. Eget duis at tellus at urna condimentum mattis pellentesque id. Pellentesque massa placerat duis ultricies lacus sed turpis. Tristique magna sit amet purus gravida. Diam vel quam elementum pulvinar.`;
+
+  container.appendChild(content);
+  return container;
+}
